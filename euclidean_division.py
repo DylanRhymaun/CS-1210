30c0daf37de0f0db30ca4b4957b3ac616d255f47
Dylan Rhymaun <rhymaundylan@gmail.com>
2024-10-11 10:54:12 -0400

    1st finished version

--- euclidean_division.py
+++ euclidean_division.py
@@ -1,8 +1,29 @@
 """
-
+Euclidean Division
 Dylan Rhymaun
-
+A program that calculates a quotient
+and remainder using euclidean division
 """
+
+def calc_quotient(r, s):
+    quotient = 0
+    while r >= s:
+        r -= s
+        quotient += 1
+    return quotient
+
+def calc_remainder(r, s):
+    while r >= s:
+        r -= s
+    return r
 
 if __name__ == '__main__':
     
+    r = int(input("Enter the dividend (a positive integer): "))
+    s = int(input("Enter the divisor (a positive integer): "))
+    
+    quotient = calc_quotient(r, s)
+    remainder = calc_remainder(r, s)
+    
+    print(f"{r} // {s} = {quotient}")
+    print(f"{r} % {s} = {remainder}")
