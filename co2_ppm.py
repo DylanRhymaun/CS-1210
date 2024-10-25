af3bce71ed4f146a76ba9761f8adfeaf577bb30c
Dylan Rhymaun <rhymaundylan@gmail.com>
2024-10-25 15:15:19 -0400

    HW 8

--- co2_ppm.py
+++ co2_ppm.py
@@ -7,17 +7,33 @@ DESC
 import csv
 import math
 
-def mean(lst):
-    # Calculate and return the mean of the argument supplied for lst.
+lst = []
+
+def calc_mean(lst):
+    return sum(lst) / len(lst)
 
 def std_dev(lst):
-    # Calculate and return the standard deviation 
-    # of the argument supplied for lst.
+    mean = calc_mean(lst)
+    variance = sum((x - mean) ** 2 for x in lst) / len(lst)  
+    return math.sqrt(variance)
    
 
 if __name__ == '__main__':
-
-    # All the rest of your code:
-    # Read data from file into a list of floats
+    # Read data from file into a lst of floats
+    with open('co2_ppm.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            lst.append(float(row[1]))
+            
     # Function call(s) to `mean()` and `std_dev()` as needed.
+    mean_value = calc_mean(lst)  
+    standard_deviation = std_dev(lst)
+    most_recent = lst[-1]
+    
     # Write results to file.
+    with open('results.txt', 'w') as results:
+        writer = csv.writer(csvfile)
+        results.write(f"Mean: {mean_value:.2f}\n")
+        results.write(f"Standard Deviation: {standard_deviation:.2f}\n")
+        results.write(f"Most recent observation: {most_recent}\n")
+
