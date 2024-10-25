39371982fffae4f14e7962fdf959eef8b951b09a
Dylan Rhymaun <rhymaundylan@gmail.com>
2024-10-24 20:04:03 -0400

    HW 8

---
+++ co2_ppm.py
@@ -0,0 +1,23 @@
+"""
+co2_ppm
+Dylan Rhymaun
+DESC
+"""
+
+import csv
+import math
+
+def mean(lst):
+    # Calculate and return the mean of the argument supplied for lst.
+
+def std_dev(lst):
+    # Calculate and return the standard deviation 
+    # of the argument supplied for lst.
+   
+
+if __name__ == '__main__':
+
+    # All the rest of your code:
+    # Read data from file into a list of floats
+    # Function call(s) to `mean()` and `std_dev()` as needed.
+    # Write results to file.
