f12e48e35deaee3cae134bf7cd120d64dfb58eab
Dylan Rhymaun <rhymaundylan@gmail.com>
2024-11-06 13:57:58 -0500

    HW 9 first submission

--- balanced.py
+++ balanced.py
@@ -1,8 +1,24 @@
 """
 Balanced
 Dylan Rhymaun
-
+Checks if an input contains balanced parenthesis
 """
 
-if __name__ == '__main__':
+stack = []
+def balanced(s):
+    for p in s:
+        if p == "(":  
+            stack.append(p)
+        elif p == ")":  
+            if not stack:  
+                return False
+            stack.pop()  
     
+    return len(stack) == 0  
+
+if __name__ == '__main__':
+    s = input("Enter a string containing parentheses: ")
+    if balanced(s) == True:
+        print("Parentheses are balanced!")
+    else:
+        print ("Parentheses are not balanced!")
--- reverse.py
+++ reverse.py
@@ -1,8 +1,17 @@
 """
 Reverse
 Dylan Rhymaun
-
+Reverses the order of characters in a provided string
 """
+
+stack = []
 
 if __name__ == '__main__':
     
+    word = (input("Enter a string, and I'll print it backwards! "))
+    for letter in word:
+        stack.append(letter)
+        
+    while stack:
+        print(stack.pop(), end='')
+    print()    
